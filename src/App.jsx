e003b26252d74fab02c44b610051a75cc891e18a
momentchan <mingjyunhung@gmail.com>
2024-03-22 14:39:51 +0800

    add test scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from '@react-three/fiber'
 import Utilities from "./r3f-gist/utility/Utilities";
+import * as handTrack from 'handtrackjs'
+
+const video = document.getElementById("myvideo");
+
+let model = null;
+
+
+function startVideo() {
+    handTrack.startVideo(video).then(function (status) {
+      console.log("video started", status);
+      if (status) {
+        console.log("Video started. Now tracking");
+        // isVideo = true;
+        // runDetection();
+      } else {
+        console.log("Please enable video");
+      }
+    });
+  }
+
+// Load the model.
+handTrack.load().then((lmodel) => {
+    // detect objects in the image.
+    model = lmodel;
+    console.log(model);
+    startVideo()
+    // updateNote.innerText = "Loaded Model!";
+    // runDetectionImage(handimg);
+    // trackButton.disabled = false;
+    // nextImageButton.disabled = false;
+  });
+  
 
 export default function App() {
     return <>
